d2f3f02dc0f3bede0f43cca4d1014668d9d057c2
Darrin Wells <designrwells@gmail.com>
2018-10-09 15:53:59 -0500

    project initialization

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PlayerEntry from './PlayerEntry.js'
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <PlayerEntry />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ PlayerEntry.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PlayerEntry extends React.Component {
+	render(){
+		return (
+			<div>
+				<button id="addPlayer">Add Player</button>
+			</div>
+			);
+	}
+}
+
+export default PlayerEntry;
